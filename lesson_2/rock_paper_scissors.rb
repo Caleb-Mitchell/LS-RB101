ceb3a67342670b3a440c7aff43df2580a6f52de6
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2021-08-27 11:40:19 -0500

    Finalize rps for code review

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -24,7 +24,7 @@ def interpret_abbrev(choice)
   when 'sp'
     'spock'
   when 's'
-    spock_or_scissors
+    spock_or_scissors()
   end
 end
 
@@ -56,19 +56,44 @@ def display_result(player, computer)
   end
 end
 
-def tally_score(player_win_bool)
+def tally_score(score, player_win_bool)
+  if player_win_bool
+    score[0] += 1
+  else
+    score[1] += 1
+  end
 end
 
-game_number = 0
-score = [0, 0]
+def display_score(score)
+  current_score = <<-MSG
+ The current score is:
+    Player: #{score[0]}
+    Computer: #{score[1]}
+  MSG
 
+  prompt(current_score)
+end
+
+def game_over?(score)
+  return true if score[0] == 3 || score[1] == 3
+end
+
+def display_game_winner(score)
+  if score[0] == 3
+    prompt("You are the grand winner!")
+  elsif score[1] == 3
+    prompt("The computer is the grand winner!")
+  end
+end
+
+score = [0, 0]
 loop do
   choice = ''
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = Kernel.gets().chomp()
 
-    if choice.size == 1 || choice.size == 2
+    if choice.size() == 1 || choice.size() == 2
       choice = interpret_abbrev(choice)
     end
 
@@ -85,9 +110,18 @@ loop do
 
   display_result(choice, computer_choice)
 
-  # prompt("Do you want to play again?")
-  # answer = Kernel.gets().chomp()
-  # break unless answer.downcase().start_with?('y')
+  tally_score(score, win?(choice, computer_choice))
+  display_score(score)
+
+  if game_over?(score)
+    display_game_winner(score)
+
+    prompt("Do you want to play again?")
+    answer = Kernel.gets().chomp()
+    break unless answer.downcase().start_with?('y')
+
+    score = [0, 0]
+  end
 end
 
 prompt("Thank you for playing. Good bye!")

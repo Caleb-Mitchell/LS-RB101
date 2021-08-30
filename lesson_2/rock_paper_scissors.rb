4fd92e908a93192528983f2535fe87294e8deddb
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2021-08-29 23:58:50 -0500

    Finalize rps_feedback branch with feedback

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,4 +1,3 @@
-VALID_CHOICES = %w(rock paper scissors lizard spock)
 WINNING_MOVES = {
   'rock' => ['scissors', 'lizard'],
   'paper' => ['rock', 'spock'],
@@ -6,35 +5,63 @@ WINNING_MOVES = {
   'spock' => ['rock', 'scissors'],
   'lizard' => ['paper', 'spock']
 }
-
-def clear_screen
-  puts "\e[H\e[2J"
-end
-
-def reset_game(score)
-  score[0] = 0
-  score[1] = 0
-  clear_screen
-end
+VALID_CHOICES = WINNING_MOVES.keys
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def clear_screen
+  puts "\e[H\e[2J"
+end
+
+def display_welcome
+  welcome_message = <<-MSG
+Welcome to Rock-Paper-Scissors-Lizard-Spock!
+   ---------------------
+   The rules are simple...
+   Choose your "hand signal", and see if you beat the computer's choice!
+   ---------------------
+   Scissors cuts Paper covers Rock crushes
+   Lizard poisons Spock smashes Scissors
+   decapitates Lizard eats Paper disproves
+   Spock vaporizes Rock crushes Scissors
+   ---------------------
+  MSG
+
+  prompt(welcome_message)
+end
+
+def start_game
+  clear_screen
+  display_welcome
+end
+
+def player_choice
+  loop do
+    prompt("Choose one: (r)ock, (p)aper, (sc)issors, (l)izard, (sp)ock")
+    choice = Kernel.gets().chomp().downcase()
+
+    if choice.size() == 1 || choice.size() == 2
+      return choice = interpret_abbrev(choice)
+    end
+
+    if VALID_CHOICES.include?(choice)
+      return choice
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+end
+
 def interpret_abbrev(choice)
   case choice
-  when 'r'
-    'rock'
-  when 'p'
-    'paper'
-  when 'l'
-    'lizard'
-  when 'sc'
-    'scissors'
-  when 'sp'
-    'spock'
-  when 's'
-    spock_or_scissors()
+  when 'r' then 'rock'
+  when 'p' then 'paper'
+  when 'l' then 'lizard'
+  when 'sc' then 'scissors'
+  when 'sp' then 'spock'
+  when 's' then spock_or_scissors()
   end
 end
 
@@ -42,6 +69,7 @@ def spock_or_scissors
   loop do
     prompt('Please input "sc" for scissors, or "sp" for spock')
     new_abbr = gets().chomp()
+
     if new_abbr == "sc"
       return "scissors"
     elsif new_abbr == "sp"
@@ -66,29 +94,12 @@ def display_result(player, computer)
   end
 end
 
-def tally_score(score, player_win_bool)
-  if player_win_bool
+def tally_score(score, choice, computer_choice)
+  if win?(choice, computer_choice)
     score[0] += 1
-  else
+  elsif win?(computer_choice, choice)
     score[1] += 1
   end
-end
-
-def display_welcome
-  welcome_message = <<-MSG
-Welcome to Rock-Paper-Scissors-Lizard-Spock!
-   ---------------------
-   The rules are simple...
-   Choose your "hand signal", and see if you beat the computer's choice!
-   ---------------------
-   Scissors cuts Paper covers Rock crushes
-   Lizard poisons Spock smashes Scissors
-   decapitates Lizard eats Paper disproves
-   Spock vaporizes Rock crushes Scissors
-   ---------------------
-  MSG
-
-  prompt(welcome_message)
 end
 
 def display_score(score)
@@ -115,29 +126,30 @@ def display_game_winner(score)
   end
 end
 
-score = [0, 0]
-
-clear_screen
-
-display_welcome
-loop do
-  choice = ''
+def play_again?
+  answer = ''
   loop do
-    # prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    prompt("Choose one: (r)ock, (p)aper, (sc)issors, (l)izard, (sp)ock")
-    choice = Kernel.gets().chomp().downcase()
-
-    if choice.size() == 1 || choice.size() == 2
-      choice = interpret_abbrev(choice)
-    end
-
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid choice.")
-    end
+    prompt("Do you want to play again? (y/n)")
+    answer = Kernel.gets().chomp().downcase()
+    break if answer == 'y' || answer == 'n'
+    puts "Please enter 'y' or 'n'"
   end
 
+  return true if answer == 'y'
+  return false if answer == 'n'
+end
+
+def reset_game(score)
+  score[0] = 0
+  score[1] = 0
+  clear_screen
+end
+
+# Main game loop
+start_game
+score = [0, 0]
+loop do
+  choice = player_choice
   computer_choice = VALID_CHOICES.sample()
 
   clear_screen
@@ -146,34 +158,17 @@ loop do
 
   display_result(choice, computer_choice)
 
-  tally_score(score, win?(choice, computer_choice))
+  tally_score(score, choice, computer_choice)
+
   display_score(score)
 
   if game_over?(score)
     display_game_winner(score)
 
-    prompt("Do you want to play again?")
-    answer = Kernel.gets().chomp().downcase()
-    break unless answer.downcase().start_with?('y')
+    break unless play_again?
 
     reset_game(score)
   end
 end
 
 prompt("Thank you for playing. Good bye!")
-
-=begin
-TODO
-- display game rules and a quick welcome before game begins***
-- clear the screen before the game starts for a "clean slate"***
-- clear the screen between rounds***
-- make user input case insensitive***
-- elimiate small repition in constants, call WINNING_MOVES.keys
-  for VALID_CHOICES
-- add an additional branch to conditional in tally_score to avoid computer being
-  awarded a point on a tie
-- extract 104-117 into a 'player choice' method
-- extra 132-143 to a 'play_again?' method (can break out of the loop with
-  something like 'break unless play_again?')
-  - make it so only 'n' or 'N' exits game at end
-=end

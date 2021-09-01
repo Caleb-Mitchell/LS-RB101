28d41a698aa978ada849c83d90117bb4f1e4ea15
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2021-09-01 01:23:42 -0500

    Work done on easy_4

--- exercises/easy_4/short_long_short.rb
+++ exercises/easy_4/short_long_short.rb
@@ -28,6 +28,20 @@ short_long_short('  ', 'x') == "x  x"
 short_long_short('x ', ' ') == " x "
 
 A:
-
+-save both strings
+-pick which is the longest
+-print short long short
 
 =end
+
+def short_long_short(str1, str2)
+  if str1.length > str2.length
+    str2 + str1 + str2
+  else
+    str1 + str2 + str1
+  end
+end
+
+puts short_long_short('abc', 'defgh') == "abcdefghabc"
+puts short_long_short('abcde', 'fgh') == "fghabcdefgh"
+puts short_long_short('', 'xyz') == "xyz"

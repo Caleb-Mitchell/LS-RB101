b5823340d6aab1f98e75b391a86fd8679fd81735
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2021-10-17 12:56:33 -0500

    Finish twenty_one before bonus features, and delete random file

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -1,3 +1,5 @@
+require 'pry'
+require 'pry-byebug'
 SUITS = %w(hearts diamonds clubs spades)
 VALUES = %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace)
 
@@ -9,50 +11,6 @@ def clear_screen
   system 'clear'
 end
 
-# rubocop:disable Metrics/MethodLength
-def display_welcome
-  system 'clear'
-  puts <<-MSG
-
-         ====== Welcome to 21! ======         
-
-                      --
-                  - Rules -
-                      --
-   - The player and dealer are both dealt 2 random cards.
-  
-   - Your goal is to get as close as you can to 21 points without going over,
-     but higher than the dealer's total.
-
-                      --
-                  - Points -
-                      --
-              Cards 2-10 = equal to face value
-       jack, queen, king = 10
-                     ace = 1 or 11
-                     
-                  ==========
-
-        puts "Press Enter to Continue"
-  MSG
-  gets
-end
-# rubocop:enable Metrics/MethodLength
-
-def num?(num_str)
-  num_str.to_i.to_s == num_str
-end
-
-def point_value(card)
-  if card == 'Ace'
-    nil
-  elsif num?(card)
-    card.to_i
-  else
-    10
-  end
-end
-
 def initialize_deck
   new_deck = []
   SUITS.each do |suit|
@@ -60,8 +18,8 @@ def initialize_deck
       card = {}
       card[:suit] = suit
       card[:value] = value
-      card[:points] = point_value(value)
       new_deck << card
+      puts "It's a tie!"
     end
   end
   new_deck
@@ -69,6 +27,10 @@ end
 
 def initialize_score
   { player: 0, dealer: 0 }
+end
+
+def initialize_hands
+  { player: [], dealer: [] }
 end
 
 def deal!(hand, deck, num_cards)
@@ -80,53 +42,49 @@ def deal!(hand, deck, num_cards)
   end
 end
 
-def join_and(arr, delimiter=', ', word='and')
-  if arr.size == 2
-    arr.join(" #{word} ")
-  else
-    arr[-1] = "#{word} #{arr.last}"
-    arr.join(delimiter)
-  end
+def deal_starting_hands(hands, deck)
+  deal!(hands[:player], deck, 2)
+  deal!(hands[:dealer], deck, 2)
 end
 
-def display_dealer_hand_with_secret(dealer_hand)
-  puts "Dealer has: #{dealer_hand[0][:value]}, and an unknown card."
+def display_welcome
+  clear_screen
+  puts <<-MSG
+
+         ====== Welcome to 21! ======         
+
+                  - Rules -
+
+   - The player and dealer are both dealt 2 random cards.
+  
+   - Your goal is to get as close as you can to 21 points,
+     without going over but higher than the dealer's total.
+
+                  - Points -
+
+              Cards 2-10 = equal to face value
+       jack, queen, king = 10
+                     ace = 1 or 11
+
+                  ==========
+
+  MSG
 end
 
-def display_current_hands(player_hand, dealer_hand, turn='player_turn')
-  player_cards = []
-  dealer_cards = []
+def card_list(hand)
+  card_list = []
 
-  player_hand.each do |card|
-    player_cards << card[:value]
+  hand.each do |card|
+    card_list << "#{card[:value]} of #{card[:suit]}"
   end
-  dealer_hand.each do |card|
-    dealer_cards << card[:value]
-  end
-
-  system 'clear'
-  puts ""
-  puts ""
-  puts "You have: #{join_and(player_cards)}"
-  if turn == 'player_turn'
-    display_dealer_hand_with_secret(dealer_hand)
-  else
-    puts "Dealer has: #{join_and(dealer_cards)}"
-  end
-  puts ""
-  puts ""
+  card_list
 end
 
-def display_current_points(score, turn='player_turn')
-  puts "Player points: #{score[:player]}"
+def card_list_with_secret(hand)
+  card_list = []
 
-  if turn == 'player_turn'
-    puts "Dealer points: ??"
-  else
-    puts "Dealer points: #{score[:dealer]}"
-  end
-  puts ""
-  puts ""
+  card_list << "#{hand[0][:value]} of #{hand[0][:suit]}"
+  card_list << "?? of ??"
 end
 
 def tally_num_aces(hand)
@@ -140,12 +98,16 @@ end
 def tally_hand_minus_aces(hand)
   hand_minus_aces = 0
   hand.each do |card|
-    hand_minus_aces += card[:points] unless card[:value] == 'Ace'
+    if ('2'..'10').to_a.include?(card[:value])
+      hand_minus_aces += card[:value].to_i
+    elsif %w(Jack Queen King).include?(card[:value])
+      hand_minus_aces += 10
+    end
   end
   hand_minus_aces
 end
 
-def totaled_score!(score, hand, player_or_dealer)
+def totaled_score(hand)
   running_total = tally_hand_minus_aces(hand)
 
   num_aces = tally_num_aces(hand)
@@ -153,58 +115,112 @@ def totaled_score!(score, hand, player_or_dealer)
     running_total += (running_total + 11) > 21 ? 1 : 11
   end
 
-  score[:player] = running_total if player_or_dealer == 'player'
-  score[:dealer] = running_total if player_or_dealer == 'dealer'
+  running_total
 end
 
-def hit!(player_hand, dealer_hand, deck, player_or_dealer)
-  if player_or_dealer == 'player'
-    deal!(player_hand, deck, 1)
-  elsif player_or_dealer == 'dealer'
-    deal!(dealer_hand, deck, 1)
+def display_game(hands, score)
+  clear_screen
+  puts <<-GAME
+
+         ============================
+
+    Player hand: #{card_list(hands[:player]).join(', ')}
+
+    Dealer hand: #{card_list(hands[:dealer]).join(', ')}
+
+                     ----
+
+               Player Points: #{score[:player]}
+               Dealer Points: #{score[:dealer]}
+
+                  ==========
+
+
+
+
+
+  GAME
+end
+
+def display_game_with_secrets(hands, score)
+  clear_screen
+  puts <<-GAME
+
+         ============================
+
+    Player hand: #{card_list(hands[:player]).join(', ')}
+
+    Dealer hand: #{card_list_with_secret(hands[:dealer]).join(', ')}
+
+                     ----
+
+               Player Points: #{score[:player]}
+               Dealer Points: ??
+
+                  ==========
+
+
+
+
+
+  GAME
+end
+
+def ask_hit_or_stay
+  player_choice = ''
+  loop do
+    prompt "Would you like to (h)it or (s)tay?"
+    player_choice = gets.chomp
+    break if ['h', 'hit', 's', 'stay'].include?(player_choice)
+    prompt "Sorry, not a valid choice, please try again.\n\n"
   end
+  player_choice
 end
 
-def busted?(score, player_or_dealer)
-  if player_or_dealer == 'player'
-    score[:player] > 21
-  elsif player_or_dealer == 'dealer'
-    score[:dealer] > 21
-  end
+def hit!(hand, deck)
+  deal!(hand, deck, 1)
 end
 
-def nobody_busted?(score)
-  score[:player] <= 21 && score[:dealer] <= 21
+def busted?(score)
+  score > 21
 end
 
-def calculate_no_bust_winner(score)
-  if score[:player] == score[:dealer]
-    'tie'
+def update_score!(score, hands)
+  score[:player] = totaled_score(hands[:player])
+  score[:dealer] = totaled_score(hands[:dealer])
+end
+
+def display_outcome(score)
+  if busted?(score[:player])
+    prompt "You busted, dealer wins."
+  elsif busted?(score[:dealer])
+    prompt "Dealer busted, you win!"
+  elsif score[:dealer] == score[:player]
+    prompt "It's a tie!"
   elsif score[:player] > score[:dealer]
-    'player'
-  else
-    'dealer'
+    prompt "You win!"
+  elsif score[:dealer] > score[:player]
+    prompt "Sorry, dealer wins."
   end
 end
 
-def calculate_game_winner(score)
-  if nobody_busted?(score)
-    calculate_no_bust_winner(score)
-  elsif busted?(score, 'player') && !busted?(score, 'dealer')
-    'dealer'
-  elsif !busted?(score, 'player') && busted?(score, 'dealer')
-    'player'
-  else
-    'tie'
-  end
+def dealer_can_stop?(score)
+  score[:dealer] >= 17 && !busted?(score[:dealer])
 end
 
-def display_game_winner(game_winner)
-  case game_winner
-  when 'player' then puts "You win!!"
-  when 'dealer' then puts "Sorry, dealer wins this time."
-  when 'tie'    then puts "Its a tie!"
-  end
+def display_dealer_hits
+  prompt "Dealer hits!\n\nPress enter to continue."
+  gets
+end
+
+def display_player_stay
+  prompt "You chose to stay.\n\nPress Enter to continue."
+  gets
+end
+
+def display_press_enter
+  prompt "Press Enter to continue."
+  gets
 end
 
 def play_again?
@@ -218,80 +234,68 @@ def play_again?
     elsif ['y', 'yes'].include?(answer)
       return true
     else
-      prompt "Invalid input, please enter (y)es or (n)o"
+      prompt "Invalid input, please enter (y)es or (n)o:"
       next
     end
   end
 end
 
-def ask_player_hit_or_stay
-  puts "hit or stay?"
-  gets.chomp
-end
-
-def dealer_can_stop?(score)
-  score[:dealer] >= 17 && !busted?(score, 'dealer')
-end
-
-player_busted = ''
 loop do
   display_welcome
-  player_busted = false
+  display_press_enter
 
   deck = initialize_deck
+  hands = initialize_hands
   score = initialize_score
-  player_hand = []
-  dealer_hand = []
 
-  deal!(player_hand, deck, 2)
-  deal!(dealer_hand, deck, 2)
-  totaled_score!(score, player_hand, 'player')
-  totaled_score!(score, dealer_hand, 'dealer')
+  # Deal cards to player and dealer
+  deal_starting_hands(hands, deck)
+
+  # Total scores of cards first dealt
+  update_score!(score, hands)
 
   # Player turn
   loop do
-    display_current_hands(player_hand, dealer_hand)
-    display_current_points(score)
+    display_game_with_secrets(hands, score)
 
-    answer = ask_player_hit_or_stay
-
-    if answer == 'hit'
-      hit!(player_hand, dealer_hand, deck, 'player')
-      totaled_score!(score, player_hand, 'player')
+    player_choice = ask_hit_or_stay
+    if player_choice.start_with?('h')
+      hit!(hands[:player], deck)
+      update_score!(score, hands)
     end
 
-    break if answer == 'stay' || busted?(score, 'player')
+    break if player_choice.start_with?('s') || busted?(score[:player])
   end
 
-  if busted?(score, 'player')
-    clear_screen
-    puts "Player busted!, you lose!"
-    display_current_points(score)
+  if busted?(score[:player])
+    # Save player_busted to true, used to skip dealer turn
     player_busted = true
-    break unless play_again?
+
+    update_score!(score, hands)
+    display_game(hands, score)
+    display_outcome(score)
   else
-    puts "You chose to stay!"
+    display_player_stay
   end
 
   # Dealer turn
-  puts "dealer turn:"
   loop do
-    break if player_busted
+    break if dealer_can_stop?(score) ||
+             busted?(score[:dealer]) ||
+             player_busted
 
-    display_current_hands(player_hand, dealer_hand, 'dealer_turn')
-    display_current_points(score, 'dealer_turn')
+    hit!(hands[:dealer], deck)
+    update_score!(score, hands)
 
-    break if dealer_can_stop?(score) || busted?(score, 'dealer')
-
-    hit!(player_hand, dealer_hand, deck, 'dealer')
-    totaled_score!(score, dealer_hand, 'dealer')
+    display_game(hands, score)
+    display_dealer_hits
   end
 
-  if player_busted == false
-    game_winner = calculate_game_winner(score)
-    display_game_winner(game_winner)
-    break unless play_again?
-  end
+  update_score!(score, hands)
+  display_game(hands, score)
+  display_outcome(score)
+
+  break unless play_again?
 end
 
-puts "thanks for playing!"
+prompt "Thanks for playing!"
